b953fcb35af242df4c15ae65c3d4ce0d09c9f39c
Nitin Patel <patel904@sheridancollege.ca>
2020-07-27 17:55:09 -0400

    First Initial

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>My First React App!!</h1>
+        <h1>My First React Appaa!!</h1>
       </div>
     );
   }
