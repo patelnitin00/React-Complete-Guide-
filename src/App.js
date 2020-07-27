711e75f6503149099f8ce40e5a0d9ba9f32ad01e
Nitin Patel <patel904@sheridancollege.ca>
2020-07-27 18:49:38 -0400

    Second Commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>My First React Appaa!!</h1>
+        <h1>Second Commit</h1>
       </div>
     );
   }
