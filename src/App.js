d6ae86c84154398c57f11eddb3e32a48c4c43f8f
Nitin Patel <patel904@sheridancollege.ca>
2020-07-28 01:16:57 -0400

    Creating a functional component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 // React Complete Guide
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Second Commit</h1>
+
+        <Person />
       </div>
     );
   }
---
+++ src/Person/Person.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const person = () => {
+    return(
+        <p>I am a person class!!</p>
+    );
+}
+
+
+export default person;
