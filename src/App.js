56fd91915d82298d4cb0a9efcd2f831aeaa33f35
Nitin Patel <patel904@sheridancollege.ca>
2020-07-28 02:09:15 -0400

    Understanding and Using State

--- src/App.js
+++ src/App.js
@@ -4,13 +4,30 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+ 
+  //special property 'state' can be used only if you extends Component, cannot be used in function or lamda function
+  //props are set and managed from outside of component
+  //state is managed from inside commponent
+  // you can use state on functional based component bu using React hooks
+
+  state = {
+    persons: [
+      {name: "Max", age: 28},
+      {name: "Manu", age:29},
+      {name: "Maria", age:22}
+    ]
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Second Commit</h1>
+        <button>Switch Name</button>
 
-        <Person name="Nitin" age="28"/>
-        <Person name="Vraj" age="22">My hobby: Plaing games..</Person>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobby: Plaing games.</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        
       </div>
     );
   }
